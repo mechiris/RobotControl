52fc8712e794a6ea7b424cfeaa8d7ff8de698013
Ryan Smith <ryan@sightmachine.com>
2017-08-28 16:42:56 -0700

    Added menu logic, need to implement sequencing still.

--- robotcontrol/robotcontrol.py
+++ robotcontrol/robotcontrol.py
@@ -1,6 +1,6 @@
-from multiprocessing import Process, Pipe, Value
-from Adafruit_PWM_Servo_Driver import PWM
-import RPi.GPIO as GPIO
+from multiprocessing import Process, Pipe, Manager, Value
+#from Adafruit_PWM_Servo_Driver import PWM
+#import RPi.GPIO as GPIO
 import datetime, time
 import pandas as pd
 import numpy as np
@@ -20,32 +20,38 @@ class RobotControl():
         pulseLength /= 4096                     # 12 bits of resolution
         pulse *= 1000
         pulse /= pulseLength
-        pwm.setPWM(channel, 0, pulse)
+        self.pwm.setPWM(channel, 0, pulse)
 
-    def RunMotor(self,counter, runMotor):
-        #Initialise the PWM device using the default address
-        pwm = PWM(0x40) #for debug: pwm = PWM(0x40, debug=True)
-
-        logging.info('Initializing servo motion')
-        with counter.get_lock():
-            counter.value =0
-        while(runMotor.value):
-            pwm.setPWM(0, 0, self.servoMin)
-            time.sleep(self.breathTime)
-            pwm.setPWM(0, 0, self.servoMax)
-            time.sleep(self.breathTime)
-            with counter.get_lock():
-                counter.value += 1
-            logging.info('Stopping motor')
-        pwm.setPWM(0,0,self.servoSlack)
+    def dataPoster(self, state):
+        print('TODO: Flesh this out')
 
     def start(self):
         logging.info('Run started')
-        self.userInputSingleServoPosition()
+        # self.parent_conn, self.child_conn = Pipe()
+        # p = Process(target=self.dataPoster, args=(self.state))
+        # p.start()
+
+        self.mainMenu()
+
+        p.join()
+        #self.userInputSingleServoPosition()
 
     def goToServoPosition(self,channel,position):
         self.pwm.setPWM(channel,0,position)
     
+    def goToTeachPointMenu(self):
+        while (True):
+            print('\n')
+            teachPoint = raw_input("Please input a teachpoint to go to.  Enter l to list available teachpoints, q to return to the main menu: ")
+            if teachPoint == 'l':
+                for pos in self.teachpoints['Position'].unique():
+                    print(pos)
+            elif teachPoint == 'q':
+                break;
+            else:
+                self.goToTeachPoint(teachPoint)
+        self.changeState(0)
+
     def goToTeachPoint(self,teachpoint):
         tp = self.teachpoints[self.teachpoints['Position'] == teachpoint]
         if tp.shape<1:
@@ -56,19 +62,35 @@ class RobotControl():
             if ~pd.isnull(position):
                 self.goToServoPosition(int(channel),int(position))
     
-    def userInputSingleServoPosition(self):
+    def goToServoPositionMenu(self):
+        servoextent = ''
         while (True):
-            servoChannel = raw_input("Please input a servo channel (0-5).  Enter q to stop: ")
+            if servoextent == 'q':
+                break;
+            servoChannel = raw_input("Please input a servo channel (0-5).  Enter q to return to the main menu: ")
             if servoChannel == 'q':
                 break;
             while(True):
-                servoextent = raw_input("Please input a servo extent from 0 to 4096, or s to switch servo channel")
+                servoextent = raw_input("Please input a servo extent from 0 to 4096, or s to switch servo channel, q to return to the main menu")
                 if servoextent == "s":
                     logging.info('Switching channels')
                     break;
+                if servoextent == "q":
+                    logging.info('Return to main menu')
+                    break;
                 else:
                     self.pwm.setPWM(int(servoChannel), 0, int(servoextent))
+        self.changeState(0)
+        
+    def runSequence(self):
+        print('running sequence')
     
+    def shutdown(self):
+        logging.info('Shutting down system')
+        self.goToTeachPoint('safety')
+        time.sleep(0.5)
+        self.goToTeachPoint('rest')
+
     def testSequence(self):
         while(True):
             self.goToTeachPoint('safety')
@@ -94,14 +116,51 @@ class RobotControl():
             self.goToTeachPoint('right_pick_hover')
             time.sleep(1)
 
+    def changeState(self, newState):
+        states = {
+        0 : 'mainMenu',
+        1 : 'runSequence',
+        2 : 'goToTeachPoint',
+        3 : 'goToServoPosition',
+        4 : 'shutdown',
+        }  
+        self.state['state'] = states[newState]
+
+        options = {
+        0 : self.mainMenu,
+        1 : self.runSequence,
+        2 : self.goToTeachPointMenu,
+        3 : self.goToServoPositionMenu,
+        4 : self.shutdown,
+        }  
+        options[newState]()
+
+
+
+    def mainMenu(self):
+        print('\n\n\n')
+        print('Main Menu:____________________________________________________________________________________')
+        print('Please enter a number to select one of the following options:')
+        print('1. Run Sequence')
+        print('2. Go To TeachPoint')
+        print('3. Go To Servo Position')
+        print('4. Shutdown')
+        newState = raw_input("Please input a number: ")
+        self.changeState(int(newState))
+        #runSequence
+        #goToTeachPoint
+        #goToServoPosition
+        #shutdown
+
     def __init__(self):
-        print('RobotControl Initializing')
+        #print('RobotControl Initializing')
         if not os.path.exists('/var/log/robotcontrol'):
             os.makedirs('/var/log/robotcontrol')
-        logging.basicConfig(filename='/var/log/autopump/robotcontrol.log',level=logging.INFO)
-
+        logging.basicConfig(filename='/var/log/robotcontrol/robotcontrol.log',level=logging.INFO)
         self.teachpoints = pd.read_excel('RobotPositions.xlsx',skiprows=1)
 
-        self.pwm = PWM(0x40) #for debug: pwm = PWM(0x40, debug=True)
-        #self.start()
-        self.state = Value('d', -1) #multiprocessing thread safe value passing
+        #self.pwm = PWM(0x40) #for debug: pwm = PWM(0x40, debug=True)
+        #self.goToTeachPoint('safety')
+        self.state = Manager().dict() #multiprocessing thread safe value passing
+        self.state['state'] = 'Initializing'
+
