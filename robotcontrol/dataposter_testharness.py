9d689c78931213a84b46dc98c6b7ea53b19dc44e
Ryan Smith <ryan@sightmachine.com>
2017-10-10 13:39:05 -0700

    Added csv changs and mkdir

--- robotcontrol/dataposter_testharness.py
+++ robotcontrol/dataposter_testharness.py
@@ -1,12 +1,17 @@
 import pandas as pd
 from dataposter import DataPoster
 from multiprocessing import Process, Pipe, Manager, Value
+import os
 
-
-teachpoints = pd.read_excel('RobotPositions.xlsx',sheetname='Teachpoints')
-sequences = pd.read_excel('RobotPositions.xlsx',sheetname='Sequences')
+teachpoints = pd.read_csv('Teachpoints.csv')
+sequences = pd.read_csv('Sequences.csv',delimiter=';')
 state = Manager().dict() #multiprocessing thread safe value passing
 state['state'] = 'runSequence'
+
+directory = '/opt/sightmachine/data/robotdata/'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
 
 dp = DataPoster()
 dp.initialize(teachpoints,sequences,state)
